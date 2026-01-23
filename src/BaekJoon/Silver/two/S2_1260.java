bf09a4d58afbce41a75dbddcc0fe9febd628544a
hyunwoo oh <hw62459930@gmail.com>
2026-01-23 20:32:43 +0900

    [FEAT] S2-1260 DFS와 BFS

--- src/BaekJoon/Silver/two/S2_1260.java
+++ src/BaekJoon/Silver/two/S2_1260.java
@@ -3,10 +3,7 @@ package BaekJoon.Silver.two;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class S2_1260 {
 	public static void main(String[] args) throws IOException {
@@ -35,18 +32,37 @@ public class S2_1260 {
 			Collections.sort(graph.get(i));
 		}
 
-		boolean[] dfsVisited = new boolean[n+1];
-		Stack<Integer> stack = new Stack<>();
-		stack.add(v);
-		while (!stack.isEmpty()) {
-			int cur = stack.pop();
-			if(dfsVisited[cur]) continue;
-			System.out.println(cur);
-			for (int i : graph.get(cur)) {
-				if (!dfsVisited[i]) {
-					dfsVisited[i] = true;
-					stack.add(i);
+		boolean[] visited = new boolean[n+1];
+		dfs(graph, visited, v);
+		System.out.println();
+
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(v);
+
+		boolean[] bfs_visited = new boolean[n+1];
+		while(!queue.isEmpty()) {
+			int node = queue.poll();
+			System.out.print(node + " ");
+			bfs_visited[node] = true;
+
+			for(int i : graph.get(node)) {
+				if(!bfs_visited[i]) {
+					queue.offer(i);
+					bfs_visited[i] = true;
 				}
+			}
+
+		}
+
+	}
+
+	public static void dfs(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int node) {
+		visited[node] = true;
+		System.out.print(node + " ");
+
+		for(int i : graph.get(node)) {
+			if(!visited[i]) {
+				dfs(graph, visited, i);
 			}
 		}
 	}
