4eea007a2746572e91cbac9d04abb9940fdde7b6
Oh Hyun Woo <hw62459930@gmail.com>
2026-01-26 11:52:29 +0900

    [D3] Title: 영준이의 카드 카운팅, Time: 100 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D3/4047. 영준이의 카드 카운팅/README.md
@@ -0,0 +1,15 @@
+# [D3] 영준이의 카드 카운팅 - 4047 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWIsY84KEPMDFAWN) 
+
+### 성능 요약
+
+메모리: 26,496 KB, 시간: 100 ms, 코드길이: 1,156 Bytes
+
+### 제출 일자
+
+2026-01-26 11:52
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4047. 영준이의 카드 카운팅/영준이의 카드 카운팅.java
@@ -0,0 +1,47 @@
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		sc.nextLine();
+
+		Map<Character, Integer> patternIndex = new HashMap<>();
+		patternIndex.put('S', 0);
+		patternIndex.put('D', 1);
+		patternIndex.put('H', 2);
+		patternIndex.put('C', 3);
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String line = sc.nextLine();
+			boolean[][] cards = new boolean[4][13];
+
+			boolean isError = false;
+
+			int[] answer = new int[] {13, 13, 13, 13};
+
+			for (int i = 0; i < line.length(); i += 3) {
+				char pattern = line.charAt(i);
+				int num = Integer.parseInt(line.substring(i + 1, i + 3));
+				int index = patternIndex.get(pattern);
+				if (cards[index][num - 1]) {
+					isError = true;
+					break;
+				}
+				cards[index][num - 1] = true;
+				answer[index] -= 1;
+			}
+			if (isError) {
+				System.out.println("#" + test_case + " ERROR");
+			} else {
+				System.out.println(
+					"#" + test_case + " " + answer[0] + " " + answer[1] + " " + answer[2] + " " + answer[3]);
+			}
+		}
+		sc.close();
+	}
+}
